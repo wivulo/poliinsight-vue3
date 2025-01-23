eda25c3969895702631a84ba263ba5e32483c304
vivulo <valtercio.ivulo@poligest.ao>
2025-01-23 23:31:59 +0100

    update - carousel updated

--- src/components/Carousel/CarouselView.vue
+++ src/components/Carousel/CarouselView.vue
@@ -1,13 +1,10 @@
 <script>
 import { imagesURL } from "./ImagesURL";
-// import { ChevronLeft, ChevronRight } from "lucide-vue";
 import Button from 'primevue/button';
 
 export default {
     name: 'CarouselView',
     components: {
-      // ChevronLeft,
-      // ChevronRight
       Button
     },
     data() {
@@ -32,127 +29,58 @@ export default {
           return 'next';
         } else if (diff === 2) {
           return 'nextRightSecond';
+        } else if (diff === 3) {
+          return 'extraRight';
         } else if (diff === this.images.length - 1) {
           return 'prev';
         } else if (diff === this.images.length - 2) {
           return 'prevLeftSecond';
+        } else if (diff === this.images.length - 3) {
+          return 'extraLeft';
         } else {
-          return diff > 2 ? 'hideRight' : 'hideLeft';
+          return diff > 3 ? 'hideRight' : 'hideLeft';
         }
       }
-    },
+    }
+
   };
 </script>
 
 <template>
-      <div class="relative">
-
+      <div class="flex relative">
         <div id="carousel" class="relative w-full">
-          <div v-for="(img, index) in images" :class="getClass(index)" :key="index">
-            <img :src="img">
+          <div class="carousel-item" v-for="(image, index) in images" :class="getClass(index)" :key="index">
+            <picture>
+              <figure>
+                <img :src="image.imageURL" :alt="image.name" />
+              </figure>
+              <figcaption>
+                <p class="flex items-center text-ellipsis-2" :title="image.name">
+                    <RouterLink :to="{name: 'event.show', params: {id: image.id}}" class="font-bold text-surface-900 cursor-pointer">
+                      {{ image.name }}
+                    </RouterLink>
+                </p>
+
+                <p class="flex flex-col gap-1 text-surface-500 truncate">
+                    <span class="flex items-center">
+                        <i class="pi pi-calendar mr-1" /> <span v-formatDate="image.startDate" :title="image.startDate"/>
+                    </span>
+                    <span :title="image.location"><i class="pi pi-map-marker"></i> {{ image.location }}</span>
+                </p>
+              </figcaption>
+            </picture>
           </div>
         </div>
 
-        <div class="buttons absolute w-full flex  justify-between pr-5">
+        <div class="absolute w-full flex  justify-between items-center pr-5 self-center z-50">
           <Button text class="soft-hover" @click="move('prev')">
-              <!-- <chevron-left :size="25" class="text-danger"/> -->
               <i class="fa fa-chevron-left"></i>
           </Button>
           <Button text class="soft-hover border-0" @click="move('next')">
-              <!-- <chevron-right :size="25" class="text-danger" /> -->
               <i class="fa fa-chevron-right"></i>
           </Button>
         </div>
       </div>
 </template>
 
-<style scoped>
-  #carousel {
-    height: 400px;
-    overflow: hidden;
-  }
-  
-  #carousel div {
-    position: absolute;
-    transition: transform 1s, left 1s, opacity 1s, z-index 0s;
-    opacity: 1;
-  }
-  
-  #carousel div img {
-    width: 400px;
-    transition: width 1s;
-  }
-  
-  #carousel div.hideLeft {
-    left: 0%;
-    opacity: 0;
-    transform: translateY(50%) translateX(-50%);
-  }
-  
-  #carousel div.hideLeft img {
-    width: 200px;
-  }
-  
-  #carousel div.hideRight {
-    left: 100%;
-    opacity: 0;
-    transform: translateY(50%) translateX(-50%);
-  }
-  
-  #carousel div.hideRight img {
-    width: 200px;
-  }
-  
-  #carousel div.prev {
-    z-index: 5;
-    left: 30%;
-    transform: translateY(50px) translateX(-50%);
-  }
-  
-  #carousel div.prev img {
-    width: 300px;
-  }
-  
-  #carousel div.prevLeftSecond {
-    z-index: 4;
-    left: 15%;
-    transform: translateY(50%) translateX(-50%);
-    opacity: .7;
-  }
-  
-  #carousel div.prevLeftSecond img {
-    width: 200px;
-  }
-  
-  #carousel div.selected {
-    z-index: 10;
-    left: 50%;
-    transform: translateY(0px) translateX(-50%);
-  }
-  
-  #carousel div.next {
-    z-index: 5;
-    left: 70%;
-    transform: translateY(50px) translateX(-50%);
-  }
-  
-  #carousel div.next img {
-    width: 300px;
-  }
-  
-  #carousel div.nextRightSecond {
-    z-index: 4;
-    left: 85%;
-    transform: translateY(50%) translateX(-50%);
-    opacity: .7;
-  }
-  
-  #carousel div.nextRightSecond img {
-    width: 200px;
-  }
-  
-  .buttons {
-    top: 35vh;
-  }
-  
-</style>
+<style scoped src="../../assets/css/carousel.css"></style>
--- src/components/Carousel/ImagesURL.js
+++ src/components/Carousel/ImagesURL.js
@@ -1,9 +1,52 @@
 export const imagesURL = [
-    'https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg',
-    'https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg',
-    'https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg',
-    'https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg',
-    'https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg',
-    'https://i1.sndcdn.com/artworks-000108468163-dp0b6y-t500x500.jpg',
-    'https://i1.sndcdn.com/artworks-000064920701-xrez5z-t500x500.jpg'
+    {
+
+        id: 1,
+        imageURL: 'https://i1.sndcdn.com/artworks-000165384395-rhrjdn-t500x500.jpg',
+        name: 'Academic Conference 2023',
+        location: 'New York, USA',
+        startDate: '2023-11-15'
+    },
+    {
+        id: 2,
+        imageURL: 'https://i1.sndcdn.com/artworks-000185743981-tuesoj-t500x500.jpg',
+        name: 'Science Symposium 2023',
+        location: 'London, UK',
+        startDate: '2023-12-01'
+    },
+    {
+        id: 3,
+        imageURL: 'https://i1.sndcdn.com/artworks-000158708482-k160g1-t500x500.jpg',
+        name: 'Tech Expo 2023',
+        location: 'San Francisco, USA',
+        startDate: '2023-10-20'
+    },
+    {
+        id: 4,
+        imageURL: 'https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg',
+        name: 'Tech Expo 2023',
+        location: 'San Francisco, USA',
+        startDate: '2023-10-20'
+    },
+    {
+        id: 5,
+        imageURL: 'https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg',
+        name: 'Tech Expo 2023',
+        location: 'San Francisco, USA',
+        startDate: '2023-10-20'
+    },
+    {
+        id: 6,
+        imageURL: 'https://i1.sndcdn.com/artworks-000062423439-lf7ll2-t500x500.jpg',
+        name: 'Tech Expo 2023',
+        location: 'San Francisco, USA',
+        startDate: '2023-10-20'
+    },
+    {
+        id: 7,
+        imageURL: 'https://i1.sndcdn.com/artworks-000028787381-1vad7y-t500x500.jpg',
+        name: 'Tech Expo 2023',
+        location: 'San Francisco, USA',
+        startDate: '2023-10-20'
+    },
 ]
