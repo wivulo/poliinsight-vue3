5a505a0cf31aa507620b99e6854221345b8feed4
vivulo <valtercio.ivulo@poligest.ao>
2025-02-26 00:27:29 +0100

    fix 0027

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ let databaseURL;
 
 console.log(process.env.NODE_ENV);
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'development') {
     databaseURL = "https://api-eventos-ispb.onrender.com";
 } else {
     databaseURL = "http://localhost:3000";
