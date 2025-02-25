5e50cea70b064fd62ced50e1e0fdbb92b8e8393e
vivulo <valtercio.ivulo@poligest.ao>
2025-02-25 13:20:17 +0100

    fix 1320

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 let databaseURL;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV) {
     databaseURL = "https://api-eventos-ispb.onrender.com";
 } else {
     databaseURL = "http://localhost:3000";
